510e06b9e543a402252db77dcb7aca2fd1e2ad8e
Samuel Debionne <samuel.debionne@esrf.fr>
2022-06-13 15:08:59 +0200

    Add gitlab CI and Conda packaging

--- conda/tango/bld.bat
+++ conda/tango/bld.bat
@@ -1,7 +1,7 @@
 cd tango
 
-cmake -Bbuild -H. -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DPYTHON_SITE_PACKAGES_DIR=%SP_DIR% -DCMAKE_FIND_ROOT_PATH=%LIBRARY_PREFIX%
+cmake -Bbuild -H. -G "Ninja" -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DCMAKE_FIND_ROOT_PATH=%LIBRARY_PREFIX%
 IF %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
 
-cmake --build build --config Release --target install
+cmake --build build  --target install
 IF %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
